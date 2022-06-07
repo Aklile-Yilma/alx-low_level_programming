e24a992bd2235a4da2453a3bda1cf5779a74b944
Aklile-Yilma <aklileyilma@gmail.com>
2022-06-06 21:51:27 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,8 @@
+#!/bin/bash
 #include <stdio.h>
-
-/**
- * main -       Use 'puts' to output a string to stdout
- *
- * Description: prints ' "Programming is like building a multilingual puzzle '
- *              to stdout
- *
- * Return:      Returns '0' if output string to stdout with no issue
- */
-
-int main(void){
-	puts("Programming is like building a multilingual puzzle");
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
