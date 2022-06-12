597426f7d6be7b2821a8b21db704d2a8a03c539c
Aklile-Yilma <aklileyilma@gmail.com>
2022-06-12 02:45:42 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * main- Entry point
+ * Return: Always 0(Success)
+ * */
+
+
+int main(void){
+
+	for(int i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	
+	}
+	putchar('\n');
+
+	return(0);
+}
+
