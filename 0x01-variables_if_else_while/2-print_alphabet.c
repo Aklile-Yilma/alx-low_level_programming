24458d05e739a335b6fae5bfa9716e7c7fb53081
Aklile-Yilma <aklileyilma@gmail.com>
2022-06-12 02:57:09 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,22 +6,15 @@
  * Return: Always 0(Success)
  * */
 
-int main(void)
-{
-	char alpha = 'a';
 
-<<<<<<< HEAD
-	while (alpha <= 'z')
-=======
 int main(void){
 	char i = 'a';
 	for(i; i <= 'z'; i++)
->>>>>>> 53776c712f9b129d5d7af44962ff8fa44e994ade
 	{
-		putchar(alpha);
-		alpha++;
+		putchar(i);
+	
 	}
 	putchar('\n');
 
-	return (0);
+	return(0);
 }
